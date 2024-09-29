9465d510d9c7ef91d7b29ba1f758fb3ad0e94241
Henry Maddocks <hmaddocks@me.com>
2024-09-30 08:21:49 +1300

    Add the Table module

---
+++ lib/toy_robot/table.ex
@@ -0,0 +1,27 @@
+defmodule ToyRobot.Table do
+  alias ToyRobot.Table
+  defstruct [:north_boundary, :east_boundary]
+
+  @doc """
+  Determines if a postion would be within the table's boundaries
+
+  ## Examples
+
+      iex> alias ToyRobot.Table
+      ToyRobot.Table
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> table |> Table.valid_position?(%{north: 4, east: 4})
+      true
+      iex> table |> Table.valid_position?(%{north: 0, east: 0})
+      true
+      iex> table |> Table.valid_position?(%{north: 6,east: 0})
+      false
+  """
+  def valid_position?(%Table{north_boundary: north_boundary, east_boundary: east_boundary}, %{
+        north: north,
+        east: east
+      }) do
+    north <= north_boundary && north >= 0 && east <= east_boundary && east >= 0
+  end
+end
