ebf93caca9f6886e023e495d63f7ad2c42815d3d
Henry Maddocks <hmaddocks@me.com>
2024-10-05 22:44:02 +1300

    Starting CommandRunner

---
+++ lib/toy_robot/command_runner.ex
@@ -0,0 +1,14 @@
+defmodule ToyRobot.CommandRunner do
+  alias ToyRobot.{Simulation, Table}
+
+  def run([{:place, placement} | rest]) do
+    table = %Table{north_boundary: 4, east_boundary: 4}
+
+    case Simulation.place(table, placement) do
+      {:ok, simulation} -> simulation
+      {:error, :invalid_placement} -> run(rest)
+    end
+  end
+
+  def run([]), do: nil
+end
---
+++ test/toy_robot/command_runner_test.exs
@@ -0,0 +1,19 @@
+defmodule ToyRobot.CommandRunnerTest do
+  use ExUnit.Case, async: true
+
+  alias ToyRobot.{CommandRunner, Simulation}
+
+  test "handles a vlaid place command" do
+    %Simulation{robot: robot} =
+      CommandRunner.run([{:place, %{east: 1, north: 2, facing: :north}}])
+
+    assert robot.east == 1
+    assert robot.north == 2
+    assert robot.facing == :north
+  end
+
+  test "handles an invalid place command" do
+    simulation = CommandRunner.run([{:place, %{east: 10, north: 10, facing: :north}}])
+    assert simulation == nil
+  end
+end
